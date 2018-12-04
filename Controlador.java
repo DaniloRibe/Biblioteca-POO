45548a3a8e89a0762905575e7744faad04b9df02
DaniloRibe <42821607+DaniloRibe@users.noreply.github.com>
2018-12-04 00:50:38 -0200

    Add files via upload

---
+++ Controlador.java
@@ -0,0 +1,272 @@
+package controller;
+
+import java.util.Collection;
+import javax.swing.JOptionPane;
+import model.Cliente;
+import model.Gerente;
+import model.Livro;
+import model.Operador;
+import model.Usuario;
+import repositories.RepositorioLivro;
+import repositories.RepositorioUsuario;
+
+public class Controlador {
+
+    RepositorioLivro repositorioLivro = new RepositorioLivro();
+    RepositorioUsuario repositorioUsuario = new RepositorioUsuario();
+    Cliente c = new Cliente();
+
+    public void adicionarLivro(String titulo, String autor, String editora, String edicao) throws ParametroVazioException {
+        if (titulo.equals("") || autor.equals("") || editora.equals("") || edicao.equals("")) {
+            throw new ParametroVazioException();
+        } else {
+            Livro l = new Livro(titulo, autor, editora, edicao);
+            repositorioLivro.getLivros().put(titulo, l);
+            JOptionPane.showMessageDialog(null, "LIVRO ADICIONADO");
+        }
+    }
+
+    public void adicionarCliente(String nome, String cpf, String email, String senha) throws ParametroVazioException {
+        if (nome.equals("") || cpf.equals("") || email.equals("") || senha.equals("")) {
+            throw new ParametroVazioException();
+        } else {
+            Cliente u = new Cliente(nome, cpf, email, senha);
+            repositorioUsuario.getUsuarios().add(u);
+            JOptionPane.showMessageDialog(null, "CLIENTE ADICIONADO");
+        }
+    }
+
+    public void adcionarGerente(String nome, String cpf, String email, String senha) {
+        Gerente u = new Gerente(nome, cpf, email, senha);
+        repositorioUsuario.getUsuarios().add(u);
+    }
+
+    public void adicionarOperador(String nome, String cpf, String email, String senha) throws ParametroVazioException {
+        if (nome.equals("") || cpf.equals("") || email.equals("") || senha.equals("")) {
+            throw new ParametroVazioException();
+        } else {
+            Operador u = new Operador(nome, cpf, email, senha);
+            repositorioUsuario.getUsuarios().add(u);
+            JOptionPane.showMessageDialog(null, "OPERADOR ADICIONADO");
+        }
+    }
+
+    public void mostrarLivros() {
+        if (repositorioLivro.getLivros().isEmpty()) {
+            JOptionPane.showMessageDialog(null, "NÃO HÁ LIVROS CADASTRADOS");
+        }
+        Collection<Livro> li = repositorioLivro.getLivros().values();
+        for (Livro l : li) {
+            System.out.println(l);
+        }
+    }
+
+    public void mostrarClientes() {
+        int cont = 0;
+        for (Usuario u : repositorioUsuario.getUsuarios()) {
+            if (u instanceof Cliente) {
+                System.out.println(u);
+                cont++;
+            }
+        }
+        if (cont == 0) {
+            JOptionPane.showMessageDialog(null, "NÃO HÁ CLIENTES CADASTRADOS");
+        }
+
+    }
+
+    public void mostrarOperadores() {
+        int cont = 0;
+        for (Usuario u : repositorioUsuario.getUsuarios()) {
+            if (u instanceof Operador) {
+                System.out.println(u);
+                cont++;
+            }
+        }
+        if (cont == 0) {
+            JOptionPane.showMessageDialog(null, "NÃO HÁ OPERADORES CADASTRADOS");
+        }
+    }
+
+    public void removerLivro(String titulo) {
+        Livro liv = repositorioLivro.getLivros().remove(titulo);
+        if (liv == null) {
+            JOptionPane.showMessageDialog(null, "LIVRO NÃO ENCONTRADO!");
+        } else {
+            JOptionPane.showMessageDialog(null, "LIVRO REMOVIDO");
+        }
+    }
+
+    public void removerOperador(String cpf) {
+        Usuario usu = null;
+        for (Usuario u : repositorioUsuario.getUsuarios()) {
+            if (u.getCpf().equals(cpf)) {
+                usu = u;
+            }
+        }
+        if (usu == null) {
+            JOptionPane.showMessageDialog(null, "OPERADOR NÃO ENCONTRADO!");
+        } else {
+            repositorioUsuario.getUsuarios().remove(usu);
+            JOptionPane.showMessageDialog(null, "OPERADOR REMOVIDO!");
+        }
+    }
+
+    public void buscarLivro(String titulo) {
+        if (repositorioLivro.getLivros().get(titulo) == null) {
+            JOptionPane.showMessageDialog(null, "LIVRO NÃO ENCONTRADO!");
+        } else {
+            System.out.println(repositorioLivro.getLivros().get(titulo));
+        }
+    }
+
+    public boolean naoExisteLivro(String titulo) {
+        boolean e = false;
+        if (repositorioLivro.getLivros().get(titulo) == null) {
+            e = true;
+        }
+        return e;
+    }
+
+    public void buscarCliente(String nome) {
+        int cont1 = 0;
+        int cont2 = 0;
+        for (Usuario u : repositorioUsuario.getUsuarios()) {
+            if (u instanceof Cliente) {
+                cont1++;
+                if (u.getNome().equals(nome)) {
+                    cont2++;
+                    System.out.println(u);
+                }
+            }
+        }
+        if (cont1 == 0 && cont2 == 0) {
+            JOptionPane.showMessageDialog(null, "NÃO EXISTEM CLIENTES CADASTRADOS!");
+        }
+        if (cont1 > 0 && cont2 == 0) {
+            JOptionPane.showMessageDialog(null, "CLIENTE NÃO ENCONTRADO!");
+        }
+    }
+
+    public void buscarOperador(String nome) {
+        int cont1 = 0;
+        int cont2 = 0;
+        for (Usuario u : repositorioUsuario.getUsuarios()) {
+            if (u instanceof Operador) {
+                cont1++;
+                if (u.getNome().equals(nome)) {
+                    cont2++;
+                    System.out.println(u);
+                }
+            }
+        }
+        if (cont1 == 0 && cont2 == 0) {
+            JOptionPane.showMessageDialog(null, "NÃO EXISTEM OPERADORES CADASTRADOS!");
+        }
+        if (cont1 > 0 && cont2 == 0) {
+            JOptionPane.showMessageDialog(null, "OPERADOR NÃO ENCONTRADO!");
+        }
+    }
+
+    public void pegarLivro(String email, String titulo) {
+        Livro l = repositorioLivro.getLivros().get(titulo);
+        for (Usuario u : repositorioUsuario.getUsuarios()) {
+            if (u instanceof Cliente) {
+                if (u.getEmail().equals(email)) {
+                    if (((Cliente) u).quantidadeLivros < 4) {
+                        ((Cliente) u).getLivros().add(l);
+                        ((Cliente) u).quantidadeLivros++;
+                        repositorioLivro.getLivros().get(titulo).setStatus("Indisponível");
+                        JOptionPane.showMessageDialog(null, "EMPRÉSTIMO REALIZADO");
+                    } else {
+                        JOptionPane.showMessageDialog(null, "QUANTIDADE MÁXIMA DE LIVROS ATINGIDA!");
+                    }
+                }
+            }
+        }
+    }
+
+    public boolean estaDisponivel(String titulo) {
+        boolean e = false;
+        Livro l = repositorioLivro.getLivros().get(titulo);
+        if (l.getStatus().equals("Disponível")) {
+            e = true;
+        }
+        return e;
+    }
+
+    public void devolverLivro(String email, String titulo) {
+        Livro l = repositorioLivro.getLivros().get(titulo);
+        for (Usuario u : repositorioUsuario.getUsuarios()) {
+            if (u instanceof Cliente) {
+                if (u.getEmail().equals(email)) {
+                    ((Cliente) u).getLivros().remove(l);
+                    ((Cliente) u).quantidadeLivros--;
+                    repositorioLivro.getLivros().get(titulo).setStatus("Disponível");
+                    JOptionPane.showMessageDialog(null, "LIVRO DEVOLVIDO!");
+                }
+            }
+        }
+    }
+
+    public void mostrarMeusLivros(String email) {
+        for (Usuario u : repositorioUsuario.getUsuarios()) {
+            if (u instanceof Cliente) {
+                if (((Cliente) u).getLivros().isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "VOCÊ NÃO TEM LIVROS!");
+                }
+                if (u.getEmail().equals(email)) {
+                    for (Livro l : ((Cliente) u).getLivros()) {
+                        System.out.println(l.getTitulo());
+                    }
+                }
+            }
+        }
+    }
+
+    public boolean existeEmail(String email) {
+        boolean e = false;
+        for (Usuario u : repositorioUsuario.getUsuarios()) {
+            if (u.getEmail().equals(email)) {
+                e = true;
+            }
+        }
+        return e;
+    }
+
+    public boolean existeCpf(String cpf) {
+        boolean e = false;
+        for (Usuario u : repositorioUsuario.getUsuarios()) {
+            if (u.getCpf().equals(cpf)) {
+                e = true;
+            }
+        }
+        return e;
+    }
+
+    public void verficarLogin(String email, String senha) {
+        int cont = 0;
+        String nome = null;
+        for (Usuario u : repositorioUsuario.getUsuarios()) {
+            if (u.getEmail().equals(email) && u.getSenha().equals(senha)) {
+                cont++;
+                nome = u.getNome();
+            }
+        }
+        if (cont == 0) {
+            JOptionPane.showMessageDialog(null, "E-MAIL OU SENHA INCORRETOS!");
+        } else {
+            System.out.println("\nBem-Vindo " + nome);
+        }
+    }
+
+    public Usuario verificarTipo(String email, String senha) {
+        Usuario u = null;
+        for (Usuario usu : repositorioUsuario.getUsuarios()) {
+            if (usu.getEmail().equals(email) && usu.getSenha().equals(senha)) {
+                u = usu;
+            }
+        }
+        return u;
+    }
+}
---
+++ Principal.java
@@ -0,0 +1,245 @@
+package exec;
+
+import controller.Controlador;
+import controller.ParametroVazioException;
+import model.Cliente;
+import model.Operador;
+import model.Usuario;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+import model.Gerente;
+
+public class Principal {
+
+    public static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Controlador c = new Controlador();
+
+        c.adcionarGerente("Danilo", "05728113303", "daniloribeiro@alu.ufc.br", "3247");
+
+        String interfaceInicial = "\n*****Bem-vindo à Biblioteca***** \n"
+                + "1 - Fazer login \n"
+                + "2 - Cadastrar-se como cliente \n"
+                + "3 - Sair";
+
+        String interfaceGerente = "\n1 - Cadastrar Operador \n"
+                + "2 - Remover Operador \n"
+                + "3 - Listar Livros \n"
+                + "4 - Listar Clientes \n"
+                + "5 - Listar Operadores \n"
+                + "6 - Procurar Livro \n"
+                + "7 - Procurar Cliente \n"
+                + "8 - Procurar Operador \n"
+                + "9 - Sair";
+
+        String interfaceOperador = "\n1 - Cadastrar Livro \n"
+                + "2 - Remover Livro \n"
+                + "3 - Listar Livros \n"
+                + "4 - Listar Clientes \n"
+                + "5 - Procurar Livro \n"
+                + "6 - Procurar Cliente \n"
+                + "7 - Sair";
+
+        String interfaceCliente = "\n1 - Listar Livros \n"
+                + "2 - Listar Meus Livros \n"
+                + "3 - Pegar Livro \n"
+                + "4 - Devolver Livro \n"
+                + "5 - Sair";
+
+        while (true) {
+            System.out.println(interfaceInicial);
+            System.out.print("Digite uma opção: ");
+            String comando = in.nextLine();
+            if (comando.equals("3")) {
+                break;
+            }
+            switch (comando) {
+                case "1":
+                    System.out.print("Digite seu email: ");
+                    String email = in.nextLine();
+                    System.out.print("Digite sua senha: ");
+                    String senha = in.nextLine();
+                    c.verficarLogin(email, senha);
+                    Usuario u = c.verificarTipo(email, senha);
+                    if (u instanceof Gerente) {
+                        System.out.println("Gerente");
+                        while (true) {
+                            System.out.println(interfaceGerente);
+                            System.out.print("Digite uma opção: ");
+                            String comandoGerente = in.nextLine();
+                            if (comandoGerente.equals("9")) {
+                                break;
+                            }
+                            switch (comandoGerente) {
+                                case "1":
+                                    System.out.print("Digite o nome: ");
+                                    String nome = in.nextLine();
+                                    System.out.print("Digite o CPF: ");
+                                    String cpf = in.nextLine();
+                                    if (c.existeCpf(cpf)) {
+                                        JOptionPane.showMessageDialog(null, "Erro: CPF já cadastrado!");
+                                        break;
+                                    }
+                                    System.out.print("Digite o e-mail: ");
+                                    String newEmail = in.nextLine();
+                                    if (c.existeEmail(newEmail)) {
+                                        JOptionPane.showMessageDialog(null, "Erro: e-mail já cadastrado!");
+                                        break;
+                                    }
+                                    System.out.print("Digite a senha: ");
+                                    String newSenha = in.nextLine();
+                                    try {
+                                        c.adicionarOperador(nome, cpf, newEmail, newSenha);
+                                    } catch (ParametroVazioException e) {
+                                        JOptionPane.showMessageDialog(null, "Erro: preencha todos os dados!");
+                                    }
+                                    break;
+                                case "2":
+                                    String operador = JOptionPane.showInputDialog("Digite o CPF");
+                                    c.removerOperador(operador);
+                                    break;
+                                case "3":
+                                    c.mostrarLivros();
+                                    break;
+                                case "4":
+                                    c.mostrarClientes();
+                                    break;
+                                case "5":
+                                    c.mostrarOperadores();
+                                    break;
+                                case "6":
+                                    String livro = JOptionPane.showInputDialog(null, "Nome do livro");
+                                    c.buscarLivro(livro);
+                                    break;
+                                case "7":
+                                    String cliente = JOptionPane.showInputDialog("Nome do cliente");
+                                    c.buscarCliente(cliente);
+                                    break;
+                                case "8":
+                                    String os = JOptionPane.showInputDialog("Nome do operador");
+                                    c.buscarOperador(os);
+                                    break;
+                                default:
+                                    JOptionPane.showMessageDialog(null, "ENTRADA INVÁLIDA");
+                                    break;
+                            }
+                        }
+                    } else if (u instanceof Cliente) {
+                        System.out.println("Cliente");
+                        while (true) {
+                            System.out.println(interfaceCliente);
+                            System.out.print("Digite uma opção: ");
+                            String comandoCliente = in.nextLine();
+                            if (comandoCliente.equals("5")) {
+                                break;
+                            }
+                            switch (comandoCliente) {
+                                case "1":
+                                    c.mostrarLivros();
+                                    break;
+                                case "2":
+                                    c.mostrarMeusLivros(email);
+                                    break;
+                                case "3":
+                                    String livro = JOptionPane.showInputDialog("Nome do livro");
+                                    if (c.naoExisteLivro(livro)) {
+                                        JOptionPane.showMessageDialog(null, "NÃO EXISTE ESTE LIVRO NA BIBLIOOTECA!");
+                                        break;
+                                    }
+                                    if (!c.estaDisponivel(livro)) {
+                                        JOptionPane.showMessageDialog(null, "ESTE LIVRO NÃO ESTÁ DISPONÍVEL!");
+                                        break;
+                                    } else {
+                                        c.pegarLivro(email, livro);
+
+                                    }
+                                    break;
+                                case "4":
+                                    String livroD = JOptionPane.showInputDialog(null, "Nome do livro");
+                                    c.devolverLivro(email, livroD);
+                                    break;
+                                default:
+                                    JOptionPane.showMessageDialog(null, "ENTRADA INVÁLIDA!");
+                                    break;
+                            }
+                        }
+                    } else if (u instanceof Operador) {
+                        System.out.println("Operador do Sistema");
+                        while (true) {
+                            System.out.println(interfaceOperador);
+                            System.out.print("Digite uma opção: ");
+                            String comandoOperador = in.nextLine();
+                            if (comandoOperador.equals("7")) {
+                                break;
+                            }
+                            switch (comandoOperador) {
+                                case "1":
+                                    System.out.print("Nome do livro: ");
+                                    String titulo = in.nextLine();
+                                    System.out.print("Autor do livro: ");
+                                    String autor = in.nextLine();
+                                    System.out.print("Editora: ");
+                                    String editora = in.nextLine();
+                                    System.out.print("Edição: ");
+                                    String edicao = in.nextLine();
+                                    try {
+                                        c.adicionarLivro(titulo, autor, editora, edicao);
+                                    } catch (ParametroVazioException e) {
+                                        JOptionPane.showMessageDialog(null, "Erro: preencha todos os dados!");
+                                    }
+                                    break;
+                                case "2":
+                                    String tituloR = JOptionPane.showInputDialog(null, "Nome do livro");
+                                    c.removerLivro(tituloR);
+                                    break;
+                                case "3":
+                                    c.mostrarLivros();
+                                    break;
+                                case "4":
+                                    c.mostrarClientes();
+                                    break;
+                                case "5":
+                                    String livroBuscar = JOptionPane.showInputDialog("Nome do livro");
+                                    c.buscarLivro(livroBuscar);
+                                    break;
+                                case "6":
+                                    String clienteBuscar = JOptionPane.showInputDialog("Nome do cliente");
+                                    c.buscarCliente(clienteBuscar);
+                                    break;
+                                default:
+                                    JOptionPane.showMessageDialog(null, "ENTRADA INVÁLIDA!");
+                            }
+                        }
+                    }
+                    break;
+                case "2":
+                    System.out.print("Digite seu nome: ");
+                    String nome = in.nextLine();
+                    System.out.print("Digite seu CPF: ");
+                    String cpf = in.nextLine();
+                    if (c.existeCpf(cpf)) {
+                        JOptionPane.showMessageDialog(null, "Erro: CPF já cadastrado!");
+                        break;
+                    }
+                    System.out.print("Digite seu email: ");
+                    String newEmail = in.nextLine();
+                    if (c.existeEmail(newEmail)) {
+                        JOptionPane.showMessageDialog(null, "Erro: e-mail já cadastrado!");
+                        break;
+                    } else {
+                        System.out.print("Digite sua senha: ");
+                        String newSenha = in.nextLine();
+                        try {
+                            c.adicionarCliente(nome, cpf, newEmail, newSenha);
+                        } catch (ParametroVazioException e) {
+                            JOptionPane.showMessageDialog(null, "Erro: preencha todos os dados!");
+                        }
+                    }
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "ENTRADA INVÁLIDA");
+            }
+        }
+    }
+}
